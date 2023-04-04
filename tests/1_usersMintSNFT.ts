c2aa07fc4fca122f6f4c4fbc1fa200af1b04a6f7
Gustbel <gustavobelbruno@gmail.com>
2023-04-04 10:32:33 -0300

    Added first test script

---
+++ tests/1_usersMintSNFT.ts
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+import { ethers } from 'hardhat';
+import { Contract, utils  } from 'ethers';
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+const sNFT_initialPrice = 0.05;
+const mint_amount_alice = 1;
+const mint_amount_bob = 20;
+
+describe(`Deploy sNFT contract`, () => {
+	let owner: SignerWithAddress;
+	let alice: SignerWithAddress, bob: SignerWithAddress;
+
+	let sNFT: Contract;
+
+	describe(`1) Init Signets and Instance all the contracts`, () => {
+		it(`Should init Signers`, async () => {
+			[owner, alice, bob] = await ethers.getSigners();
+		});
+		it(`Should deploy the ERC20`, async () => {
+			let SNFT = await ethers.getContractFactory("sNFT");
+			sNFT = await SNFT.deploy();
+		});
+		it(`Should Alice Mint ${mint_amount_alice} sNFT`, async () => {
+			await sNFT.connect(alice).mint( 1, { value: ethers.utils.parseEther(`${mint_amount_alice * sNFT_initialPrice}`) } );
+		});
+		it(`Should BOB Mint ${mint_amount_bob} sNFT`, async () => {
+			await sNFT.connect(alice).mint( 20, { value: ethers.utils.parseEther(`${mint_amount_bob * sNFT_initialPrice}`) } );
+		});
+		it(`Should totalSupply() be equal to the ingresed Ether value`, async () => {
+			let totalSupply = await sNFT.totalSupply();
+    		expect(await totalSupply).to.equal( 
+				ethers.utils.parseEther(`${(mint_amount_alice+mint_amount_bob) * sNFT_initialPrice}`));
+		});
+	});
+});
